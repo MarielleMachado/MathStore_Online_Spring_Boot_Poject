9a9b87bb576b27091207b118975f8b005a768bda
MarielleMachado <162848331+MarielleMachado@users.noreply.github.com>
2024-05-19 12:55:45 -0600

    :(

--- src/main/java/com/MathStore/tiendita_online/backend/BackendApplication.java
+++ src/main/java/com/MathStore/tiendita_online/backend/BackendApplication.java
@@ -2,12 +2,13 @@ package com.MathStore.tiendita_online.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.MathStore.tiendita_online.backend")
+@EnableJpaRepositories("com.MathStore.tiendita_online.backend.repository")
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
+
--- src/main/java/com/MathStore/tiendita_online/backend/infrastucture/adapter/IUserCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package com.MathStore.tiendita_online.backend.infrastucture.adapter;
-
-import com.MathStore.tiendita_online.backend.infrastucture.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IUserCrudRepository extends CrudRepository<UserEntity,Integer> {
-
-}
